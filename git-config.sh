c8e2348fde4658ce4225f837d4fe15b371376bc7
Nicolas Stadler <stadlernicolas26@gmail.com>
2026-03-24 19:38:21 +0100

    Initial Commit

--- git-config.sh
+++ git-config.sh
@@ -1,2 +1,7 @@
+GITHUB_EMAIL="stadlernicolas26@gmail.com"
+GITHUB_FULLNAME="Nicolas Stadler"
+GITHUB_USERNAME="stad-nico"
+
 git config --global user.email "stadlernicolas26@gmail.com"
 git config --global user.name "Nicolas Stadler"
+git config --global url."git@github.com:${GITHUB_USERNAME}/".insteadOf "https://github.com/${GITHUB_USERNAME}/"
