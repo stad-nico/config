541eccfb1e7ba4de125948973119794bf9793085
stad-nico <69627127+stad-nico@users.noreply.github.com>
2026-03-24 21:27:22 +0100

    Update repository clone path in installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "[Bootstrap] Cloning repository..."
-git clone "https://github.com/stad-nico/config.git" "$HOME/repositories"
+git clone "https://github.com/stad-nico/config.git" "$HOME/repositories/config"
 
-cd "$HOME/repositories"
+cd "$HOME/repositories/config"
 echo "[Bootstrap] Starting configuration..."
 bash ./main.sh
